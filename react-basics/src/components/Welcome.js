42c41821ebaa57b59f6be1cd418b138e889d1156
RISHABHXVAZ <srivastavarishabh799@gmail.com>
2025-10-08 10:09:48 +0530

    Created Welcome Component using arrow function

--- react-basics/src/components/Welcome.js
+++ react-basics/src/components/Welcome.js
@@ -1,7 +1,9 @@
 
-function Welcome(){
-    return <h1>Welcome World...</h1>
+// function Welcome(){
+//     return <h1>Welcome World...</h1>
     
-}
+// }
+
+const Welcome = () => <h1>Welcome,World..</h1>
 
 export default Welcome
